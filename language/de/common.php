7ece0ce0e92cd38fbcd37ea20963cb8ea11d2a63
towen <towenpa@gmail.com>
2016-03-16 08:53:26 -0400

    Agregado idioma aleman

---
+++ language/de/common.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * Edit Log
+ * @copyright (c) 2016 towen - [towenpa@gmail.com]
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * German translation / Deutsche Übersetzung: Frank Ingermann [info@frankingermann.de]
+ */
+
+if (!defined('IN_PHPBB'))
+{
+    exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+    $lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'VIEW_EDIT_LOG'			=> ' <a href="%1s">(Versions-Historie)</a>',
+    'NO_EDIT_LOG'			=> 'Versionshitorie nicht speichern',
+
+    'ACTUAL_MESSAGE'			=> 'aktuell',
+    'COMPARE_VERSIONS'			=> 'Vergleiche Versionen',
+
+    'EDIT_LOG'			=> 'Versions-Historie',
+    'POST_TITLE'			=> 'Beitrag',
+
+    'OLD'		=> 'alt',
+    'NEW'		=> 'neu',
+    'USER'		=> 'Benutzer',
+    'EDIT_TIME'				=> 'Bearbeitungszeitpunkt',
+    'EDIT_REASON'	=> 'Bearbeitungsgrund',
+
+    'ORIGINAL_MESSAGE'			=> 'Original-Beitrag',
+    'TEXT_CHANGES'	=> 'Änderungen im Text',
+
+    'EDIT_OPTIONS_EQUALS'			=> 'Bitte wähle zwei verschiedene Versionen zum Vergleichen aus.',
+    'NO_EDIT_OPTIONS'			=> 'Bitte wähle zwei Versionen zum Vergleichen aus.',
+    'NO_POST_LOG'			=> 'Zu diesem Beitrag gibt es keine Versions-Historie. <a href="%1s">Zurück zum Beitrag</a>',
+    'EDITLOG_NO_AUTH'			=> 'Du darfst die Versions-Historie dieses Beitrags nicht einsehen. <a href="%1s">Zurück zum Beitrag</a>',
+    'EDITLOG_NO_DELETE_AUTH'	=> 'Du darfst keine Einträge aus der Versions-Historie löschen. <a href="%1s">Zurück</a>',
+    'EDITLOG_DELETE_SUCCESS'	=> 'Entries were deleted from edit . <a href="%1s">Back</a>',
+    'LOG_EDITLOG_DELETE_SUCCESS'	=> '<strong>Deleted editions history entry </strong><br />» <a href="%1s">%s2s</a>',
+));

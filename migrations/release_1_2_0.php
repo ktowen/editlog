b37dd53f24ba91facea32c67596088e21faec6c4
towen <towenpa@gmail.com>
2016-10-05 01:29:01 -0400

    Agregada migracion para la nueva version

---
+++ migrations/release_1_2_0.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * Edit Log
+ * @copyright (c) 2016 towen - [towenpa@gmail.com]
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace towen\editlog\migrations;
+
+class release_1_2_0 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['towen_editlog_version']) && version_compare($this->config['towen_editlog_version'], '1.2.0', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\towen\editlog\migrations\release_1_1_1');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.update', array('towen_editlog_version', '1.2.0')),
+
+			array('permission.permission_set', array('ROLE_MOD_FULL', 'm_delete_editlog')),
+			array('permission.permission_set', array('ROLE_MOD_FULL', 'm_view_editlog')),
+			array('permission.permission_set', array('ROLE_MOD_QUEUE', 'm_view_editlog')),
+			array('permission.permission_set', array('ROLE_MOD_SIMPLE', 'm_view_editlog')),
+			array('permission.permission_set', array('ROLE_MOD_STANDARD', 'm_view_editlog')),
+			array('permission.permission_set', array('ROLE_USER_FULL', 'u_no_editlog')),
+
+		);
+	}
+}

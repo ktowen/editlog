c094acc0e25f745343903b825ed59d5a829d58c1
towen <towenpa@gmail.com>
2016-10-04 21:12:37 -0400

    Agregado idioma estonio. Gracias Mikk

---
+++ language/et/common.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ * Edit Log
+ * @copyright (c) 2016 towen - [towenpa@gmail.com]
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * Estonian translation by Mikk - [et.translations@phpbbeesti.net] - August, 2016
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+    exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+    $lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'VIEW_EDIT_LOG'			=> ' <a href="%1s">(Muudatuste logi)</a>',
+    'NO_EDIT_LOG'			=> 'Ära salvesta muudatuste ajalugu',
+
+    'COMPARE_VERSIONS'			=> 'Võrdluse versioonid',
+
+    'EDIT_LOG'			=> 'Muudatuste logi',
+    'POST_TITLE'			=> 'Postitus',
+
+    'USER'		=> 'Kasutaja',
+    'EDIT_TIME'				=> 'Muudatuse aeg',
+    'EDIT_REASON'	=> 'Muudatuse põhjus',
+
+    'ORIGINAL_MESSAGE'			=> 'Algne sõnum',
+    'TEXT_CHANGES'	=> 'Muudatused tekstis',
+
+    'EDITLOG_BAD_OPTIONS_COUNT'			=> 'Sa pead valima võrdlemiseks kaks versiooni.',
+    'NO_POST_LOG'			=> 'Sellel sõnumil ei ole salvestatud muudatusi ajalukku. <a href="%1s">Tagasi sõnumi juurde</a>',
+    'EDITLOG_NO_AUTH'			=> 'Sul ei ole õigusi vaadata muudatuse ajalugu antud postitusel. <a href="%1s">Tagasi sõnumi juurde</a>',
+    'EDITLOG_NO_DELETE_AUTH'	=> 'Sul ei ole õigusi kustutada muudatuste kirjeid ajaloos. <a href="%1s">Tagasi</a>',
+    'EDITLOG_DELETE_SUCCESS'	=> 'Kirjed on kustutatud muudatustest. <a href="%1s">Tagasi</a>',
+    'LOG_EDITLOG_DELETE_SUCCESS'	=> '<strong>Kustutatud muudatuste ajaloo kirje </strong><br />» <a href="%1s">%s2s</a>',
+));
